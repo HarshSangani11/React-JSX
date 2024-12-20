c6345a42895780ede5d8ff741c3cf71d907faf10
HarshSangani11 <harshsangani2105@gmail.com>
2024-12-20 16:18:48 +0530

    Fire-project

--- Firebase/FIREBASE/src/Firebase/Dashboard.jsx
+++ Firebase/FIREBASE/src/Firebase/Dashboard.jsx
@@ -1,54 +1,149 @@
 import { onAuthStateChanged } from 'firebase/auth'
 import React, { useEffect, useState } from 'react'
 import { auth, db } from '../../firebaseConfig'
-import { doc, getDoc } from 'firebase/firestore'
-import {CircleLoader} from 'react-spinners'
+import { addDoc, collection, deleteDoc, doc, getDoc, updateDoc, where } from 'firebase/firestore'
+import { CircleLoader } from 'react-spinners'
+import { getDocs } from 'firebase/firestore';
+import { data, Link, useNavigate } from 'react-router-dom'
+
 export default function Dashboard() {
-  const [user,setUser]=useState()
-  const [userData,setUserData]=useState()
-  useEffect(()=>{
-    onAuthStateChanged(auth,(data)=>{
-      if(data){
+  const navigate = useNavigate()
+  const [user, setUser] = useState()
+  const [userData, setUserData] = useState()
+  const [name, setName] = useState("")
+  const [price, setPrice] = useState("")
+  const [editIndex, setEditIndex] = useState(null)
+  const [record, setRecord] = useState()
+  useEffect(() => {
+    onAuthStateChanged(auth, (data) => {
+      if (data) {
         setUser(data.uid);
       }
     })
-  },[])
-  useEffect(()=>{
+  }, [])
+  useEffect(() => {
     fetchUser()
-  },[user])
-  const fetchUser=async()=>{
-    if(user){
-      await getDoc(doc(db,"Users",user)).then((data)=>{
+    fetchData()
+  }, [user])
+  const fetchUser = async () => {
+    if (user) {
+      await getDoc(doc(db, "Users", user)).then((data) => {
         setUserData(data.data())
         console.log(data.data());
-        
+
       })
     }
   }
-  return (
+  const fetchData = async () => {
+    await getDocs(collection(db, "data")).then((data) => {
+      ;
+      let sameData = data.docs.filter((item) => item.data().uid == user)
+      let details = sameData.map((item) => ({
+        docId: item.id,
+        ...item.data(),
+      }));
+      setRecord(details);
+    })
+  };
 
-  <div >
-  {userData ? (
-    <div className="main">
-      <h2>Welcome to Dashboard!</h2>
-      {/* <h1>Welcome, {userData.name}!</h1> */}
-      <ul>
-        <li>
-          <strong>Name:</strong>  {userData.name}
-        </li>
-        <li>
-          <strong>City:</strong> {userData.city}
-        </li>
-        <li>
-          <strong>Age:</strong> {userData.age}
-        </li>
-        <li>
-          <strong>Email:</strong> {userData.email}
-        </li>
-      </ul>
+  const handlesubmit = async () => {
+    if (editIndex == null) {
+      await addDoc(collection(db, "data"), { name, price, uid: user })
+      setName("")
+      setPrice("")
+      fetchData()
+    }
+    else {
+      await updateDoc(doc(db, "data", editIndex), { name, price, uid: user })
+      fetchData()
+    }
+    setEditIndex(null)
+  }
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "data", id))
+      .then((data) => {
+        let remainingRecord = record.filter((item) => item.docId != id)
+        setRecord(remainingRecord)
+      })
+  }
+  const handleEdit = async (id) => {
+    let singledata = record.find((item) => item.docId == id)
+    setEditIndex(id)
+    setName(singledata.name)
+    setPrice(singledata.price)
+  }
+  const Logout = async () => {
+    await auth.signOut()
+    navigate("/login")
+  }
+
+  return (
+    <div>
+      {userData ? (
+        <div>
+        <div className="dashboard-main">
+          <h2>Welcome to the Dashboard!</h2>
+          <ul className="user-info">
+            <li>
+              <strong>Name:</strong> {userData.name}
+            </li>
+            <li>
+              <strong>City:</strong> {userData.city}
+            </li>
+            <li>
+              <strong>Age:</strong> {userData.age}
+            </li>
+            <li>
+              <strong>Email:</strong> {userData.email}
+            </li>
+          </ul>
+          <button onClick={Logout}>Logout
+          </button>
+        </div>
+        <div className="form-container">
+        <input
+          type="text"
+          placeholder="Enter Product Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Enter Product Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <button onClick={handlesubmit}>{editIndex ? 'Update' : 'Add Data'}</button>
+      </div>
+
+        </div>
+      ) : (
+        <div className='loder'>
+          <CircleLoader />
+        </div>
+        
+      )}
+
+     
+     
+      <div className="rm">
+        {record &&
+          record.map((item) => (
+            <div key={item.docId} className="record-list">
+              <ul className="record-item">
+                <li><strong>UID:</strong> {item.uid}</li>
+                <li><strong>Name:</strong> {item.name}</li>
+                <li><strong>Price:</strong> ${item.price}</li>
+                <li>
+                  <button onClick={() => handleEdit(item.docId)}>Edit</button>
+                  <button onClick={() => handleDelete(item.docId)}>Delete</button>
+                </li>
+              </ul>
+            </div>
+          ))
+        }
+      </div>
     </div>
-  ) : (
-<CircleLoader/>  )}
-</div>
-  )
+      
+  );
 }
--- Firebase/FIREBASE/src/Firebase/Singup.jsx
+++ Firebase/FIREBASE/src/Firebase/Singup.jsx
@@ -6,36 +6,36 @@ import { Link, useNavigate } from 'react-router-dom';
 import { doc, setDoc } from 'firebase/firestore';
 
 export default function Singup() {
-    const [name,setName]=useState();
-    const [city,setCity]=useState();
-    const [age,setAge]=useState();
-    const [email,setEmail]=useState();
-    const [password,setPassword]=useState();
-    const navigate=useNavigate()
-    const handleSingUp= async()=>{
-        let user=await createUserWithEmailAndPassword(auth,email,password)
-        .then((data)=>{
-          setDoc(doc(db,"Users",data.user.uid),{name,city,age,email})
+  const [name, setName] = useState();
+  const [city, setCity] = useState();
+  const [age, setAge] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate()
+  const handleSingUp = async () => {
+    let user = await createUserWithEmailAndPassword(auth, email, password)
+      .then((data) => {
+        setDoc(doc(db, "Users", data.user.uid), { name, city, age, email })
         console.log(data)
         navigate("/dashboard")
-    }
-  )
+      }
+      )
   }
   return (
     <div>
-       <div className="div">
-       <h1>Sing Up</h1>
-        <input type="text" placeholder='Enter Name' onChange={(e)=>setName(e.target.value)}/>
-        <input type="text" placeholder='Enter City' onChange={(e)=>setCity(e.target.value)}/>
-        <input type="text" placeholder='Enter Age' onChange={(e)=>setAge(e.target.value)}/>
-        <input type="email" placeholder='Enter Email' onChange={(e)=>setEmail(e.target.value)}/>
-        <input type="text" placeholder='Enter Password' onChange={(e)=>setPassword(e.target.value)}/>
+      <div className="form-container-1">
+        <h1>Sing Up</h1>
+        <input type="text" placeholder='Enter Name' onChange={(e) => setName(e.target.value)} />
+        <input type="text" placeholder='Enter City' onChange={(e) => setCity(e.target.value)} />
+        <input type="text" placeholder='Enter Age' onChange={(e) => setAge(e.target.value)} />
+        <input type="email" placeholder='Enter Email' onChange={(e) => setEmail(e.target.value)} />
+        <input type="text" placeholder='Enter Password' onChange={(e) => setPassword(e.target.value)} />
         <button onClick={handleSingUp}>Sing Up</button>
         <span>
-        <Link to={"/login"}>SingIn</Link>
+          <Link to={"/login"}>SingIn</Link>
 
         </span>
-       </div>
+      </div>
     </div>
   )
 }
